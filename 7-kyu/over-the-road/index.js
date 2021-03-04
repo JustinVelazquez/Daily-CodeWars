2d90b9fe176a64ce146ea368adc1ca1d64693a53
Justin V <justinv4994@gmail.com>
2021-03-04 16:57:40 -0500

    codewars

---
+++ 7-kyu/over-the-road/index.js
@@ -0,0 +1,6 @@
+function overTheRoad(address, n) {
+  //we take in both the address of our house and the length of each row of houses
+  // we want to return a digit which will represent
+
+  return n * 2 + 1 - address;
+}
