bac8ea2f1c7949089a6f24b10df211f278352732
Justin V <justinv4994@gmail.com>
2021-03-06 11:22:41 -0500

    codewars

---
+++ 7-kyu/poem-formatter/index.js
@@ -0,0 +1,8 @@
+function formatPoem(poem) {
+  //your code here
+  poem.split('. ').join('.\n');
+}
+
+formatPoem(
+  'Beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex. Complex is better than complicated.'
+);
