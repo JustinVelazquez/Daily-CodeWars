d0d64e7248766328f62424ce5ba7fd6af9ba636e
Justin V <justinv4994@gmail.com>
2021-03-17 23:14:22 -0400

    3-17-21

---
+++ 7-kyu/Coding-Meetup-Series/3-Is-Ruby-Coming/index-test.js
@@ -0,0 +1,48 @@
+var list1 = [
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+  },
+  {
+    firstName: 'Madison',
+    lastName: 'U.',
+    country: 'United States',
+    continent: 'Americas',
+    age: 32,
+    language: 'Ruby',
+  },
+];
+
+var list2 = [
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+  },
+];
+
+Test.assertEquals(isRubyComing(list1), true);
+Test.assertEquals(isRubyComing(list2), false);
