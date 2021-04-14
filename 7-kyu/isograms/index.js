3484b5f5622c1cc9fb88c3c17daade3ddebd1d9b
Justin V <justinv4994@gmail.com>
2021-04-14 16:24:47 -0400

    daily codewars

---
+++ 7-kyu/isograms/index.js
@@ -0,0 +1,16 @@
+function isIsogram(str) {
+  if (str === '') {
+    return true;
+  }
+
+  let i, j;
+  str = str.toLowerCase();
+  for (i = 0; i < str.length; ++i) {
+    for (j = i + 1; j < str.length; ++j) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
