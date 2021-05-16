f35e3001422ead9ddd9c01e6e2b5e9176ff20fe0
Justin V <justinv4994@gmail.com>
2021-05-16 19:23:50 -0400

    codewars

---
+++ 7-kyu/Coding-Meetup-Series/12-github-admin/index.js
@@ -0,0 +1,6 @@
+function findAdmin(list, lang) {
+  // thank you for checking out the Coding Meetup kata :)
+  return list.filter(
+    (dev) => dev.language === lang && dev.githubAdmin === 'yes'
+  );
+}
