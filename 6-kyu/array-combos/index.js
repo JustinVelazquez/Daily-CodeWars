0b001d7df90d981efc32633000fc56fa042ae469
Justin V <justinv4994@gmail.com>
2021-06-20 12:30:53 -0400

    codewars

---
+++ 6-kyu/array-combos/index.js
@@ -0,0 +1,13 @@
+// const solve = (a) => a.map((v) => new Set(v).size).reduce((v, w) => v * w, 1);
+
+// const solve = (arr) => arr.reduce((res, a) => res * new Set(a).size, 1);
+
+// function solve(arr) {
+//       return new Set(curr).size * acc;
+//     }, 1);
+//   };
+
+function solve(arr) {
+  const setArr = arr.map((arr) => Array.from(new Set(arr)));
+  return setArr.reduce((count, arr) => count * arr.length, 1);
+}
