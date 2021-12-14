3ddb855cbbb7252f409a5f0ef34b75e7dd1b2961
Justin V <justinv4994@gmail.com>
2021-12-14 16:05:03 -0500

    daily CW

---
+++ 7-kyu/add-property/index.js
@@ -0,0 +1,3 @@
+questions.map((object) => (object.usersAnswer = null));
+
+// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
