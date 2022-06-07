c5d052c6566f84de05a1c3a0466f2b16d7a50fc6
Justin V <justinv4994@gmail.com>
2022-06-07 16:19:43 -0400

    daily CW

---
+++ 6-kyu/dont-rely-on-luck/index.js
@@ -0,0 +1,14 @@
+// var guess = this.args
+// console.log(Math.floor(Math.random() * 100 + 1))
+Math.random = function (n) {
+  return 0;
+};
+var guess = 1;
+
+// The test fixture I use for this kata is pre-populated.
+
+// It will compare your guess to a random number generated using:
+
+// Math.floor(Math.random() * 100 + 1)
+
+// You can pass by relying on luck or skill but try not to
