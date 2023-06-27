c148b991bde20f71fe52d0dbfef0bf1c62105925
Justin V <justinv4994@gmail.com>
2023-06-27 13:22:29 -0400

    daily CW

---
+++ 7-kyu/string-reordering/index.js
@@ -0,0 +1,19 @@
+// Return the values as a string-seperated sentence in the order of their keys' integer equivalent (increasing order).
+
+// The keys are not reoccurring and their range is -999 < key < 999. The dictionaries' keys & values will always be strings and will always not be empty.
+// Example
+
+// Input:
+// List = [
+//         {'4': 'dog' }, {'2': 'took'}, {'3': 'his'},
+//         {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
+//        ]
+
+// Output:
+// 'Vatsan took his dog for a spin'
+
+function sentence(List) {
+  return List.sort((a, b) => Object.keys(a) - Object.keys(b))
+    .map((item) => Object.values(item))
+    .join(' ');
+}
